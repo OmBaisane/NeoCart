9183dc3f3daa2ebaae1b036c611208ed4082ab4f
Om Baisane <ombaisane29@gmail.com>
2026-01-10 17:24:32 +0530

    Secure db.php: rename to db.sample.php, remove real credentials

--- config/db.sample.php
+++ config/db.sample.php
@@ -1,8 +1,22 @@
 <?php
+<<<<<<< HEAD
 $host = "YOUR_HOST";
 $username = "YOUR_DB_USER";
 $password = "YOUR_DB_PASSWORD";
 $database = "YOUR_DB_NAME";
+=======
+// config/db.php
+
+// Session start karo sabse pehle
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$host = "localhost";
+$username = "root"; 
+$password = "";
+$database = "neocart";
+>>>>>>> 8733675 (Secure db.php: rename to db.sample.php, remove real credentials)
 
 $conn = new mysqli($host, $username, $password, $database);
 
@@ -10,5 +24,9 @@ if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+<<<<<<< HEAD
+=======
+// Set charset to prevent encoding issues
+>>>>>>> 8733675 (Secure db.php: rename to db.sample.php, remove real credentials)
 $conn->set_charset("utf8mb4");
 ?>
